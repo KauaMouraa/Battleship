3e9d8446d8e7428d32dc861f478100fe80b6b97b
KauaMouraa <kauamoura.dev@gmail.com>
2025-05-01 14:54:38 -0300

    add ship placement

--- navelbatlle.c
+++ navelbatlle.c
@@ -7,7 +7,15 @@ void main() {
     int x, y;
     char tabuleiro[10][10];
 
+    // Local inicial do barco na Vertical
+    int linha_inicialV = 2;
+    int colunaV = 4;
 
+    //Local inicial do barco da horizontal
+    int linha_inicialH = 8;
+    int colunaH = 7;
+
+    //criando o tabuleiro
     for (x = 0; x < 10; x++)
     {
         for (y = 0; y < 10; y++)
@@ -16,6 +24,17 @@ void main() {
         }     
     }
 
+    //adicionando barco na vertical
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[linha_inicialV + i][colunaV] = '3';
+    }
+
+    //adicionando barco na horizontal
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[linha_inicialH][colunaH + i] = '3';
+    }
+
+    //Exibindo o tabuleiro
     for (x = 0; x < 10; x++) {
         for (y = 0; y < 10; y++) {
             printf("%c ", tabuleiro[x][y]);
